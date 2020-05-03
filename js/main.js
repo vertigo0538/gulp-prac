91f5388a2cc50e770d9fa6aaf281edd64e70bc3b
KHK <ston0538@gmail.com>
2020-05-04 02:28:04 +0900

    Update 2020-05-03T17:27:58.675Z

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-"use strict";var _util=require("./util"),test=(0,_util.add)(2,3);console.log(test);
-
-},{"./util":2}],2:[function(require,module,exports){
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.add=void 0;var add=function(e,d){return e+d};exports.add=add;
+"use strict";
 
 },{}]},{},[1]);
